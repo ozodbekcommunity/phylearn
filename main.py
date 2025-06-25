6191e69d2de6c981c125e13fa6c54ee95f5994b2
jumayevdev <haqnazarjumayev111@gmail.com>
2025-06-25 07:47:45 +0500

    changed

--- main.py
+++ main.py
@@ -1797,7 +1797,7 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8001,
         reload=settings.DEBUG
     )
